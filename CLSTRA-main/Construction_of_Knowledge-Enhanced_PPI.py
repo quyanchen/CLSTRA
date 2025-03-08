d47548fdbf3dadba5c0f9bfb0a93232a146f4e01
quyanchen <83582277+quyanchen@users.noreply.github.com>
2025-03-08 14:51:26 +0800

    Update Construction_of_Knowledge-Enhanced_PPI.py

--- CLSTRA-main/Construction_of_Knowledge-Enhanced_PPI.py
+++ CLSTRA-main/Construction_of_Knowledge-Enhanced_PPI.py
@@ -28,18 +28,6 @@ with open(go_cc_tt) as f:
             cc_tt.append(tt_t)
             protein_cc_tt[protein] = cc_tt
 print("protein_cc_tt")
-
-file1 = open("dataset/go_slim_mapping.tab.txt")
-go_tag_c = set()
-for i in file1:
-    i = i.rstrip('\n')
-    cfp = i.split('  ')[0].split('\t')
-    go_tag_1 = cfp[3]
-    if go_tag_1 == "C":
-        go_tag_c.add(cfp[4])
-GO_CC = list(go_tag_c)
-GO_CC = sorted(GO_CC, key=str.lower)
-
 print("GO_CC")
 
 protein_map = {}
@@ -61,21 +49,6 @@ for key, value in protein_cc_tt.items():
     protein_map[key] = map_value
 
 print("protein_map")
-
-with open('dataset/Krogan14K/krogan14k_co.txt', 'r') as p:
-    for lin in p:
-        line = lin.rstrip().split(' ')
-        if line[0] in protein_map and line[1] in protein_map:
-            c_t_0 = protein_map[line[0]]
-            c_t_1 = protein_map[line[1]]
-            c_t = [list(set(c_t_0[0]) & set(c_t_1[0])), list(set(c_t_0[1]) & set(c_t_1[1]))]
-            flag = [m*12+n+1 for m in c_t[0] for n in c_t[1]]
-            if len(flag) != 0:
-                with open('dataset/Krogan14K/krogan14k_re.txt', 'a') as d:
-                    d.write(line[0] + " " + line[1] + " " + str(flag) + "\n")
-
-print("k14_re")
-
 with open('dataset/Krogan14K/krogan14k_re.txt', 'r') as r, \
      open('dataset/Krogan14K/krogan14k_edges_marked.txt', 'w') as out:
     for lin in r:
